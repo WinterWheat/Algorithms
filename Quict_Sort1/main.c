4442428e6cc8b480e7c14ce98775db577da9a467
WinterWheat <877894241@qq.com>
2014-10-07 18:55:34 +0800

    Quicksort algorithms realized using C language.
    Quick Sort algorithm realized using C language.
    By default we choose the last element as sentry in partion.

---
+++ Quict_Sort1/main.c
@@ -0,0 +1,40 @@
+/**************************************************************************************************/
+/*  Copyright (C)   USTC@2014-2015    pengjiade@gmail.com                                         */
+/*                                                                                                */
+/*  FILE NAME             :  main.c                                                               */
+/*  PRINCIPAL AUTHOR      :  PengJiade                                                            */
+/*  SUBSYSTEM NAME        :  quicksort                                                            */
+/*  MODULE NAME           :  quicksort                                                            */
+/*  LANGUAGE              :  C                                                                    */
+/*  TARGET ENVIRONMENT    :  ANY                                                                  */
+/*  DATE OF FIRST RELEASE :  2014/10/07                                                           */
+/*  DESCRIPTION           :  quick sort algorithms                                                */
+/**************************************************************************************************/
+/*
+ * Revision log:
+ *
+ * Created by PengJiade, 2014/10/07
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "quicksort.h"
+
+int main()
+{
+    int i;
+    int arr[10] = {2, 3, 4, 7, 8, 1, 9, 0, 5, 6};
+    printf("before sort:\n");
+    for(i = 0;i < 10; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    QuickSort(arr, 0, 9);
+    printf("\nafter sort:\n");
+    for(i = 0;i < 10; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
---
+++ Quict_Sort1/quicksort.c
@@ -0,0 +1,45 @@
+#include "quicksort.h"
+
+int QuickSort(int *arr, int p, int r)
+{
+    int q;
+    if(arr == NULL)
+    {
+        return -1;
+    }
+    if(p < r)
+    {
+        q = Partition(arr, p, r);
+        QuickSort(arr, p, q-1);
+        QuickSort(arr, q+1, r);
+    }
+    return 0;
+}
+
+int Partition(int *arr, int p, int r)
+{
+    if(arr == NULL)
+    {
+        return -1;
+    }
+    int i, j;
+    int sentry;
+    int tmp;
+    sentry = arr[r];
+    i = p-1;
+    for(j = p; j < r; j++)
+    {
+        if(arr[j] < sentry)
+        {
+            i = i+1;
+            tmp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = tmp;
+        }
+    }
+    i = i+1;
+    tmp = arr[r];
+    arr[r] = arr[i];
+    arr[i] = tmp;
+    return i;
+}
